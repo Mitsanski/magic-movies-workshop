57cd1201f02ea141d12734f4ada0d2f877cf503a
Kiril Mitsanski <mitsanskikiril@outlook.com>
2024-11-07 17:28:42 +0200

    Hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,21 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+
+const Salt_Rounds = 10;
 
 const userSchema = new Schema({
 	email: String,
 	password: String,
 });
 
-const User = model("User", userSchema);
 
+// hash password before save
+userSchema.pre("save", async function () {
+
+	const hash = await bcrypt.hash(this.password, Salt_Rounds);
+	this.password = hash;
+});
+
+const User = model("User", userSchema);
 
 export default User;
