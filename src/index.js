a07242faf7b9a0cfbcfb63629a2d942df6054be8
Kiril Mitsanski <mitsanskikiril@outlook.com>
2024-12-15 11:26:50 +0200

    Setup express and added default route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('works')
+import express from "express";
+
+const app = express();
+
+app.listen(5000, () =>
+	console.log(`Server is listening on http://localhost:5000...`)
+);
+
+app.get('/', (req, res) => {
+    res.send('Just works')
+})
