97689917b4426d254625568655afa8b93351333a
Kiril Mitsanski <mitsanskikiril@outlook.com>
2024-12-15 12:24:22 +0200

    Setup handlebars

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ JS Back-End Course Workshop @Softuni
 ## Developing Steps
     1. Initial Project Architecture `npm init -y`
     2. Setup Express
+    3. Setup View engine
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import handlebars from "express-handlebars";
 
 const app = express();
 
-app.listen(5000, () =>
-	console.log(`Server is listening on http://localhost:5000...`)
+app.engine(
+	"hbs",
+	handlebars.engine({
+		extname: "hbs",
+	})
 );
 
-app.get('/', (req, res) => {
-    res.send('Just works')
-})
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+app.listen(5000, () => console.log(`Server is listening on http://localhost:5000...`));
+
+app.get("/", (req, res) => {
+	res.render("index", { layout: false });
+});
